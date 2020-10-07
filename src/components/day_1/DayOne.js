25c1e9c8931c5f0929bb69d61a635ca46381c980
Suyog Adhikari <suyogxda@gmail.com>
2020-10-07 17:35:48 +0545

    Fix missing </span>

--- src/components/day_1/DayOne.js
+++ src/components/day_1/DayOne.js
@@ -30,7 +30,9 @@ class DayOne extends Component {
       react coding and template guide. So, you can create an issue...which will
       help me learn from my mistakes. Anyway, Seeya (^_^)
       <br /> <br />
-      P.S: Some code still needs to be commented and formatted properly. So, bear with it for now.
+      P.S: Some code still needs to be commented and formatted properly. So,
+      bear with it for now.
+    </span>
   );
   render() {
     return (
