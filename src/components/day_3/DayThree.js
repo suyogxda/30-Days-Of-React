e65522c3911c6b24a3da11f50d2524fbab40f52b
Suyog Adhikari <suyogxda@gmail.com>
2020-10-07 17:31:38 +0545

    Link day 3 with day 4

--- src/components/day_3/DayThree.js
+++ src/components/day_3/DayThree.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import DayFour from "../day_4/DayFour";
 
 class DayThree extends Component {
   render() {
-    return <div className="day_one_index">Alakazam</div>;
+    return <DayFour />;
   }
 }
 export default DayThree;
