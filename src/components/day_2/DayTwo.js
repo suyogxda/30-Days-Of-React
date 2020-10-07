5486e15b75b84acfcd96dbbdcf017fe16af342c0
Suyog Adhikari <suyogxda@gmail.com>
2020-10-07 17:31:16 +0545

    Add day 2 content

--- src/components/day_2/DayTwo.js
+++ src/components/day_2/DayTwo.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import WritingPad from "../ui_components/writing_pad/WritingPad";
 
 class DayTwo extends Component {
+  noteTopic = "30 Days of React";
+  noteSubTopic = "#Day 2";
+  noteContent = (
+    <span>
+      Day 2 was all about answering questions, and writing some JSX I guess.
+      <br />
+      <br />
+      So, there is nothing much to see here. Go to the next page.
+    </span>
+  );
   render() {
-    return <div className="day_one_index">Alakazam</div>;
+    return (
+      <WritingPad
+        noteTopic={this.noteTopic}
+        noteSubTopic={this.noteSubTopic}
+        noteContent={this.noteContent}
+      />
+    );
   }
 }
 export default DayTwo;
